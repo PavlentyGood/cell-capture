39a126d659218e8dfded200352079f35b1b8d0e2
pavlentygood <pavlentygood@yandex.ru>
2024-08-21 23:17:42 +0300

    implement PartyFactory

--- domain/src/main/kotlin/ru/pavlentygood/cellcapture/domain/PartyFactory.kt
+++ domain/src/main/kotlin/ru/pavlentygood/cellcapture/domain/PartyFactory.kt
@@ -1,11 +1,12 @@
 package ru.pavlentygood.cellcapture.domain
 
+import java.util.*
+
 class Party(
     val id: PartyId
 )
 
 class PartyFactory {
-    fun create(): Party {
-        TODO("Not yet implemented")
-    }
+    fun create() =
+        Party(PartyId(UUID.randomUUID()))
 }
---
+++ domain/src/test/kotlin/ru/pavlentygood/cellcapture/domain/PartyFactoryTest.kt
@@ -0,0 +1,17 @@
+package ru.pavlentygood.cellcapture.domain
+
+import io.kotest.matchers.string.shouldContain
+import org.junit.jupiter.api.Test
+
+class PartyFactoryTest {
+
+    @Test
+    fun `create party`() {
+        val partyFactory = PartyFactory()
+
+        val party: Party = partyFactory.create()
+
+        party.id.toString() shouldContain "[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}"
+            .toRegex()
+    }
+}
