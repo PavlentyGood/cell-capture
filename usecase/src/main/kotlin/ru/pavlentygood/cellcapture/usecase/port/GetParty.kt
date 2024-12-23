045710a7d5b6f025a5a34563f691611490474400
pavlentygood <pavlentygood@yandex.ru>
2024-12-24 01:09:34 +0300

    refactor output ports

--- usecase/src/main/kotlin/ru/pavlentygood/cellcapture/usecase/port/GetParty.kt
+++ usecase/src/main/kotlin/ru/pavlentygood/cellcapture/usecase/port/GetParty.kt
@@ -3,6 +3,4 @@ package ru.pavlentygood.cellcapture.usecase.port
 import ru.pavlentygood.cellcapture.domain.Party
 import ru.pavlentygood.cellcapture.domain.PartyId
 
-fun interface GetParty {
-    operator fun invoke(partyId: PartyId): Party?
-}
+fun interface GetParty : (PartyId) -> Party?
--- usecase/src/main/kotlin/ru/pavlentygood/cellcapture/usecase/port/SaveParty.kt
+++ usecase/src/main/kotlin/ru/pavlentygood/cellcapture/usecase/port/SaveParty.kt
@@ -2,6 +2,4 @@ package ru.pavlentygood.cellcapture.usecase.port
 
 import ru.pavlentygood.cellcapture.domain.Party
 
-fun interface SaveParty {
-    operator fun invoke(party: Party)
-}
+fun interface SaveParty : (Party) -> Unit
