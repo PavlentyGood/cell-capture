4cfa3403fdb197c02e5ab38018c2c41bd45911cb
pavlentygood <pavlentygood@yandex.ru>
2025-02-12 01:09:57 +0300

    buildSrc: refactor dependencies file

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,7 +1,9 @@
+@file:Suppress("ConstPropertyName")
+
 object Module {
-    const val kernel = ":kernel"
-    const val lobby = ":lobby"
-    const val game = ":game"
+    private const val kernel = ":kernel"
+    private const val lobby = ":lobby"
+    private const val game = ":game"
 
     const val kernelDomain = "$kernel:domain"
 
@@ -20,12 +22,10 @@ object Version {
     const val kotlin = "1.9.23"
     const val detekt = "1.23.6"
     const val springBoot = "3.3.2"
-    const val springDependencyManagement = "1.1.6"
     const val jackson = "2.17.2"
     const val junit = "5.11.0"
     const val kotest = "5.9.0"
     const val mockk = "1.13.12"
-    const val h2 = "2.3.232"
     const val arrow = "1.2.4"
     const val kotestArrow = "1.4.0"
     const val archUnit = "1.3.0"
@@ -35,18 +35,14 @@ object Plugin {
     const val jvm = "org.jetbrains.kotlin.jvm"
     const val detekt = "io.gitlab.arturbosch.detekt"
     const val detektFormatting = "io.gitlab.arturbosch.detekt:detekt-formatting"
-    const val springBoot = "org.springframework.boot"
-    const val springDependencyManagement = "io.spring.dependency-management"
     const val kotlinSpring = "org.jetbrains.kotlin.plugin.spring"
     const val kotlinJpa = "org.jetbrains.kotlin.plugin.jpa"
 }
 
 object Lib {
-    const val h2 = "com.h2database:h2:${Version.h2}"
     const val kotlinReflect = "org.jetbrains.kotlin:kotlin-reflect:${Version.kotlin}"
     const val jacksonKotlin = "com.fasterxml.jackson.module:jackson-module-kotlin:${Version.jackson}"
     const val springBootStarterWeb = "org.springframework.boot:spring-boot-starter-web:${Version.springBoot}"
-    const val springBootStarterJpa = "org.springframework.boot:spring-boot-starter-data-jpa:${Version.springBoot}"
     const val springBootStarterTest = "org.springframework.boot:spring-boot-starter-test:${Version.springBoot}"
     const val junitEngine = "org.junit.jupiter:junit-jupiter-engine:${Version.junit}"
     const val kotestJUnit = "io.kotest:kotest-runner-junit5:${Version.kotest}"
