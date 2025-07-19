160967b0c784223c217c128f8dfa222a8f5f1b84
pavlentygood <pavlentygood@yandex.ru>
2025-07-19 20:17:20 +0300

    game: refactoring Config

--- game/app/src/main/kotlin/ru/pavlentygood/cellcapture/game/app/Config.kt
+++ game/app/src/main/kotlin/ru/pavlentygood/cellcapture/game/app/Config.kt
@@ -1,10 +1,8 @@
 package ru.pavlentygood.cellcapture.game.app
 
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 import ru.pavlentygood.cellcapture.game.domain.CreateParty
 import ru.pavlentygood.cellcapture.game.domain.RestoreParty
 import ru.pavlentygood.cellcapture.game.listening.ListeningConfig
@@ -14,42 +12,20 @@ import ru.pavlentygood.cellcapture.game.usecase.CaptureCellsUseCase
 import ru.pavlentygood.cellcapture.game.usecase.CreatePartyUseCase
 import ru.pavlentygood.cellcapture.game.usecase.GetPartyByPlayerUseCase
 import ru.pavlentygood.cellcapture.game.usecase.RollUseCase
-import ru.pavlentygood.cellcapture.game.usecase.port.GetPartyByPlayer
-import ru.pavlentygood.cellcapture.game.usecase.port.SaveParty
 
 @Configuration
 @ComponentScan("ru.pavlentygood.cellcapture.game.rest")
 @Import(
     value = [
+        CreatePartyUseCase::class,
+        RollUseCase::class,
+        CaptureCellsUseCase::class,
         GetPartyByPlayerUseCase::class,
+        CreateParty::class,
+        RestoreParty::class,
+        SavePartyToDatabase::class,
+        GetPartyByPlayerFromDatabase::class,
         ListeningConfig::class
     ]
 )
-class Config {
-
-    @Bean
-    fun createParty(getPartyByPlayer: GetPartyByPlayer, saveParty: SaveParty) =
-        CreatePartyUseCase(getPartyByPlayer, partyFactory(), saveParty)
-
-    @Bean
-    fun partyFactory() = CreateParty()
-
-    @Bean
-    fun restoreParty() = RestoreParty()
-
-    @Bean
-    fun saveParty(jdbcTemplate: NamedParameterJdbcTemplate) =
-        SavePartyToDatabase(jdbcTemplate)
-
-    @Bean
-    fun getPartyByPlayer(jdbcTemplate: NamedParameterJdbcTemplate) =
-        GetPartyByPlayerFromDatabase(jdbcTemplate, restoreParty())
-
-    @Bean
-    fun captureCells(getPartyByPlayer: GetPartyByPlayer, saveParty: SaveParty) =
-        CaptureCellsUseCase(getPartyByPlayer, saveParty)
-
-    @Bean
-    fun roll(getPartyByPlayer: GetPartyByPlayer, saveParty: SaveParty) =
-        RollUseCase(getPartyByPlayer, saveParty)
-}
+class Config
