ff27f088feb60ed6fbe60ccb67ad7676a352e066
pavlentygood <pavlentygood@yandex.ru>
2025-07-21 01:20:54 +0300

    tests: remove redundant dependencies

--- tests/build.gradle.kts
+++ tests/build.gradle.kts
@@ -11,35 +11,21 @@ dependencyManagement {
 
 dependencies {
     implementation(project(Module.kernelDomain))
-    implementation(project(Module.lobbyDomain))
-    implementation(project(Module.lobbyUseCase))
     implementation(project(Module.lobbyRestApi))
-    implementation(project(Module.lobbyRestEndpoint))
-    implementation(project(Module.lobbyPersistence))
-    implementation(project(Module.lobbyPublishing))
     implementation(project(Module.gameRestApi))
 
     implementation(Lib.kotlinReflect)
     implementation(Lib.jacksonKotlin)
     implementation(Lib.springBootStarterWeb)
-    implementation(Lib.springBootStarterDataJpa)
     implementation(Lib.springCloudStarterFeign)
-    implementation(Lib.springKafka)
 
     testImplementation(Lib.springBootStarterTest)
     testImplementation(Lib.kotestJUnit)
     testImplementation(Lib.kotestArrow)
     testImplementation(Lib.arrow)
-    testImplementation(Lib.archUnit)
     testImplementation(Lib.testcontainersJUnit)
-    testImplementation(Lib.testcontainersPostgresql)
-    testImplementation(Lib.testcontainersKafka)
 
     testRuntimeOnly(Lib.junitEngine)
 
     testImplementation(testFixtures(project(Module.kernelDomain)))
-    testImplementation(testFixtures(project(Module.lobbyDomain)))
-    testImplementation(testFixtures(project(Module.lobbyRestEndpoint)))
-    testImplementation(testFixtures(project(Module.lobbyPersistence)))
-    testImplementation(testFixtures(project(Module.lobbyPublishing)))
 }
