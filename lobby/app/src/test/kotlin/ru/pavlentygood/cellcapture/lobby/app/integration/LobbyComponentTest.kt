cd089d10a38616b287c2a3cce94bbd3471d083da
pavlentygood <pavlentygood@yandex.ru>
2025-08-21 02:15:48 +0300

    project: add dirties context to lobby component test

--- lobby/app/src/test/kotlin/ru/pavlentygood/cellcapture/lobby/app/integration/LobbyComponentTest.kt
+++ lobby/app/src/test/kotlin/ru/pavlentygood/cellcapture/lobby/app/integration/LobbyComponentTest.kt
@@ -11,6 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.annotation.Import
 import org.springframework.http.MediaType
 import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.jdbc.Sql
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.post
@@ -32,6 +33,7 @@ import java.util.concurrent.CountDownLatch
 import java.util.concurrent.TimeUnit
 
 @SpringBootTest
+@DirtiesContext
 @AutoConfigureMockMvc
 @Import(value = [TestConsumerConfig::class])
 @Sql(
